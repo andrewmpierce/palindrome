84fd045932a0d0d5f118b75397e601eee304dfbe
apierce1992 <apierce1992@gmail.com>
2015-09-02 14:17:51 -0400

    Made palindrome using if statement

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,15 @@
-def main(palindrome):
-    return palindrome.strip('? . ! , " '').lower().replace(' ','')
-
+def main():
+    sentence = input("What is your sentence?\n")
+    sentence1 = sentence.strip('?.!,').lower().replace(' ','')
+    return sentence1
 
 def is_palindrome():
-    if 
+    string = main()
+    reverse = string[::-1]
+    if reverse == string:
+        return print("palindrome")
+    else:
+        return print("Not")
+
+
+is_palindrome()
