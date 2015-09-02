b1d85dd09c2676a725d77e6e266b1f947b347669
apierce1992 <apierce1992@gmail.com>
2015-09-02 19:11:18 -0400

    Finished normal mode.

--- palindrome.py
+++ palindrome.py
@@ -2,30 +2,21 @@ import re
 
 def main():
     sentence = input("What is your sentence?\n")
-    stripped = re.sub(r'[^A-Za-z]','', sentence).lower()
-    return stripped
+    return sentence
 
 def is_palindrome(sentence):
     stripped = re.sub(r'[^A-Za-z]','', sentence).lower()
-    #stripped = main()
     if len(stripped) <= 1:
         return True
     elif stripped[0] == stripped[-1]:
         return is_palindrome(stripped[1:-1])
     else: return False
 
+if __name__ == '__main__':
+    is_palindrome(main())
 
-#racecar
-
-    #reverse = stripped[::-1]
-    #if reverse == stripped:
-    #    return print("True")#True
-    #else:
-    #    return print("False")#False
-
-
-
-#if __name__ == '__main__':
-#    main()
-
-#is_palindrome()
+#reverse = stripped[::-1]
+#if reverse == stripped:
+#    return print("True")#True
+#else:
+#    return print("False")#False
